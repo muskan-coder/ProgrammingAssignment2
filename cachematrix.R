fa5e529104bcc9f87cd31abe319813012975ff38
muskan-coder <53108749+muskan-coder@users.noreply.github.com>
2019-07-20 13:54:59 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Problem Statement
+#Given Functions
+#The first function, makeVector creates a special "vector", which is really a list containing a function
+#set the value of the vector
+#get the value of the vector
+#set the value of the mean
+#get the value of the mean
 
-## Write a short comment describing this function
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
+#function calculates the mean of the special "vector" created with the above function
+#Given Function
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
 
+#Problem Statement
+## Caching the Inverse of a Matrix:
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Creating Special object for storing the matrix and cache its inverse.
+#This function creates a special "matrix" object that can cache its inverse.
+
+cachedMatrix <- function(x = matrix()) {
+  invers <- NULL
+  set <- function(y) {
+    x <<- y
+    invers <<- NULL
+  }
+  get <- function() x
+  Inverseset <- function(inverse) invers <<- inverse
+  Inverseget <- function() invers
+  list(set = set,
+       get = get,
+       Inverseset = Inverseset,
+       Inverseget = Inverseget)
 }
 
 
-## Write a short comment describing this function
+#function to the computation of the inverse of the matrix which was created as Cached Matrix.
+#it sends the inverse based on the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+solution <- function(x, ...) {
+  ## Return the inverse of the input matrix
+  invers <- x$Inverseget()
+  if (!is.null(invers)) {
+    message("cached data")
+    return(invers)
+  }
+  
+  mat <- x$get()
+  invers <- solve(mat, ...)
+  x$Inverseset(invers)
+  invers
 }
+
